56679a1fe00181eb87395b42ed951c1ceae8130a
Dev-Caua-Ricken <cauaricken@gmail.com>
2026-02-13 19:11:44 -0300

    to do list, logica finalizada

--- to-do list/src/App.jsx
+++ to-do list/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
 
   const [list, setList] = useState('')
   const [listCom, setListCom] = useState([])
   const [messagem, setMessagem] = useState('')
+
+  const [task, setTask] = useState(0)
 
   const next = () => {
     if(list.trim() === '') return
@@ -23,16 +25,40 @@ function App() {
   // check
   const handleCheck = (index) => {
     const newList = [...listCom]
-    
+    const wasCompleted = newList[index].value
     newList[index].value = !newList[index].value
+
+    if(!wasCompleted){
+      setTask((prev) => prev + 1)
+    }else {
+      setTask((prev) => prev - 1)
+    }
+
     setListCom(newList)
+    setMessagem('Mensagem concluida ✅')
+
+    setTimeout(() => {
+      setMessagem('')
+    }, 2000)
   }
 
 
   // delete
   const handleDelete = (index) => {
+  const completed = listCom[index].value
+
   const newList = listCom.filter((_, i) => i !== index)
   setListCom(newList)
+
+  if(completed === true) {
+    setTask((prev) => prev - 1)
+  }
+
+  setMessagem('messagem deletada com sucesso ❌')
+
+  setTimeout(() => {
+    setMessagem('')
+  }, 2000)
   }
 
   return (
@@ -43,11 +69,11 @@ function App() {
         <button onClick={next}>✖️</button>
       </div>
 
-      {messagem && <p>{messagem}</p>}
+      {messagem && <p className='message'>{messagem}</p>}
 
       <div className="map">
         {listCom.map((tables, index) => (
-          <p key={index} style={{ backgroundColor: tables.value ? '#ccc' : '#fff' }}>
+          <p key={index} style={{ backgroundColor: tables.value ? '#818181' : '#fff' }}>
             {tables.text}
             <i onClick={() => handleCheck(index)} class="fa-solid fa-circle-check check" ></i>
             <i onClick={() => handleDelete(index)} class="fa-solid fa-circle-xmark error"></i>
@@ -57,8 +83,10 @@ function App() {
       </div>
 
 
-      <p>{listCom.length}</p>
-
+      <div className="result">
+      <p>Total: {listCom.length}</p>
+      <p>Realizadas: {task}</p>
+      </div>  
 
     </div>
   )
